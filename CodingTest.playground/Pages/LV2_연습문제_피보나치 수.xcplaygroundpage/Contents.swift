7ec7926305c1cee8ff2a0188eeb2706b54931035
Yoonds <wkzks123@naver.com>
2025-06-17 20:32:07 +0900

    init

---
+++ CodingTest.playground/Pages/LV2_연습문제_피보나치 수.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+func solution(_ n: Int) -> Int {
+    var a = 0, b = 1
+    for _ in 2...n {
+        let temp = (a + b) % 1234567
+        a = b
+        b = temp
+    }
+    return n == 0 ? a : b
+}
+
+print(solution(5))
